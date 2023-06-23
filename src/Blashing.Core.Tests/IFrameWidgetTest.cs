980a1762dee4748f91315193e3506fdb2026dfca
Alex Hedley <alex.hedley@me.com>
2023-06-23 21:49:46 +0100

    #5 iFrame

---
+++ src/Blashing.Core.Tests/IFrameWidgetTest.cs
@@ -0,0 +1,39 @@
+﻿using Bunit;
+
+using Blashing.Core.Components.IFrame;
+
+namespace Blashing.Core.Tests;
+
+public class IFrameWidgetTest : TestContext
+{
+    [Fact]
+    public void IFramekWidgetMarkupShouldContainPassedInValues()
+    {
+        var url = "https://www.alexhedley.com/";
+
+        var cut = RenderComponent<IFrameWidget>(parameters => parameters
+                .Add(p => p.Url, url)
+            );
+
+        //var widget = @"
+        //    <div class=""widget-iframe"">
+        //        <iframe src="https://www.alexhedley.com/" frameborder="0"></iframe>
+        //    </div>";
+
+        var expectedIFrameMarkup = $"<iframe src=\"{url}\" frameborder=\"0\"></iframe>";
+        cut.FindAll("iframe")[0].MarkupMatches(expectedIFrameMarkup);
+    }
+
+    [Fact]
+    public void IFramekWidgetShouldContainPassedInValues()
+    {
+        var url = "https://www.alexhedley.com/";
+
+        var cut = RenderComponent<IFrameWidget>(parameters => parameters
+                .Add(p => p.Url, url)
+            );
+
+        var IFrameWidget = cut.Instance;
+        Assert.Equal(IFrameWidget.Url, url);
+    }
+}
