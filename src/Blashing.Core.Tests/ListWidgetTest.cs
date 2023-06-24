f175cba35231c1ca3cae7a7004c8b16f0edf7399
Alex Hedley <alex.hedley@me.com>
2023-06-24 10:55:23 +0100

    #7 List

---
+++ src/Blashing.Core.Tests/ListWidgetTest.cs
@@ -0,0 +1,82 @@
+﻿using Bunit;
+
+using Blashing.Core.Components.List;
+
+namespace Blashing.Core.Tests;
+
+public class ListWidgetTest : TestContext
+{
+    [Fact]
+    public void TextWidgetMarkupShouldContainPassedInValues()
+    {
+        var title = "a";
+        var moreInfo = "c";
+        var updatedAtMessage = "d";
+        var items = new List<(string label, string value)>() { ("a", "b"), ("c", "d") };
+
+        var cut = RenderComponent<ListWidget>(parameters => parameters
+                .Add(p => p.Title, title)
+                .Add(p => p.MoreInfo, moreInfo)
+                .Add(p => p.UpdatedAtMessage, updatedAtMessage)
+                .Add(p => p.Items, items)
+            );
+        
+        // var widget = @"
+        //     <div class=""widget-list"">
+        //     <h1 class=""title"">List Title</h1>
+        //         <ol>
+        //             <li>
+        //                 <span class=""label"">a</span>
+        //                 <span class=""value"">b</span>
+        //             </li>
+        //             <li>
+        //                 <span class=""label"">c</span>
+        //                 <span class=""value"">d</span>
+        //             </li>
+        //         </ol>
+        //         <ul class=""list-nostyle"">
+        //             <li>
+        //                 <span class=""label"">a</span>
+        //                 <span class=""value"">b</span>
+        //             </li>
+        //             <li>
+        //                 <span class=""label"">c</span>
+        //                 <span class=""value"">d</span>
+        //             </li>
+        //         </ul>
+        //         <p class=""more-info"">List Title More Info</p>
+        //         <p class=""updated-at"">24 June 2023</p>
+        //     </div>";
+
+        var expectedTitleMarkup = $"<h1 class=\"title\">{title}</h1>";
+        cut.FindAll("h1")[0].MarkupMatches(expectedTitleMarkup);
+
+        var expectedMoreInfoMarkup = $"<p class=\"more-info\">{moreInfo}</p>";
+        cut.FindAll("p")[0].MarkupMatches(expectedMoreInfoMarkup);
+
+        var expectedUpdatedAtMessageMarkup = $"<p class=\"updated-at\">{updatedAtMessage}</p>";
+        cut.FindAll("p")[1].MarkupMatches(expectedUpdatedAtMessageMarkup);
+    }
+
+    [Fact]
+    public void TextWidgetShouldContainPassedInValues()
+    {
+        var title = "a";
+        var moreInfo = "c";
+        var updatedAtMessage = "d";
+        var items = new List<(string label, string value)>() { ("a", "b"), ("c", "d") };
+
+        var cut = RenderComponent<ListWidget>(parameters => parameters
+            .Add(p => p.Title, title)
+            .Add(p => p.MoreInfo, moreInfo)
+            .Add(p => p.UpdatedAtMessage, updatedAtMessage)
+            .Add(p => p.Items, items)
+        );
+
+        var textWidget = cut.Instance;
+        Assert.Equal(textWidget.Title, title);
+        Assert.Equal(textWidget.MoreInfo, moreInfo);
+        Assert.Equal(textWidget.UpdatedAtMessage, updatedAtMessage);
+        Assert.Equal(textWidget.Items, items);
+    }
+}
