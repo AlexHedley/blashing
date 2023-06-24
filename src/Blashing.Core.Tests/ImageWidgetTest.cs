f4a1905936a4c7cb94f8dec98f2b1ba07c37f48f
Alex Hedley <alex.hedley@me.com>
2023-06-24 10:17:06 +0100

    #6 Image

---
+++ src/Blashing.Core.Tests/ImageWidgetTest.cs
@@ -0,0 +1,44 @@
+﻿using Bunit;
+
+using Blashing.Core.Components.Image;
+
+namespace Blashing.Core.Tests;
+
+public class ImageWidgetTest : TestContext
+{
+    [Fact]
+    public void ImageWidgetMarkupShouldContainPassedInValues()
+    {
+        var src = "https://www.alexhedley.com/images/icon-myapps.png";
+        var width = "100px";
+
+        var cut = RenderComponent<ImageWidget>(parameters => parameters
+            .Add(p => p.Src, src)
+            .Add(p => p.Width, width)
+        );
+
+        //var widget = @"
+        //    <div class=""widget-image"">
+        //      <img data-bind-src=""image | prepend '/assets'"" src=""@Src"" width="@Width"/>
+        //    </div>";
+
+        var expectedImageMarkup = $"<img data-bind-src=\"image | prepend '/assets'\" src=\"{src}\" width=\"{width}\" />";
+        cut.FindAll("img")[0].MarkupMatches(expectedImageMarkup);
+    }
+
+    [Fact]
+    public void ClockWidgetShouldContainPassedInValues()
+    {
+        var src = "https://www.alexhedley.com/images/icon-myapps.png";
+        var width = "100px";
+
+        var cut = RenderComponent<ImageWidget>(parameters => parameters
+            .Add(p => p.Src, src)
+            .Add(p => p.Width, width)
+        );
+
+        var imageWidget = cut.Instance;
+        Assert.Equal(imageWidget.Src, src);
+        Assert.Equal(imageWidget.Width, width);
+    }
+}
