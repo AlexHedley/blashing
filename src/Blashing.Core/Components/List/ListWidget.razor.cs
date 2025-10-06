6051e211a7ee635efa4c3729f021b44cd5951fac
Alex Hedley <79629950+alex-hedley@users.noreply.github.com>
2025-10-06 13:54:09 +0100

    #7 add background colour to list

---
+++ src/Blashing.Core/Components/List/ListWidget.razor.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Blashing.Core.Components.List;
+
+public partial class ListWidget : BaseWidget
+{
+    [Parameter]
+    public string? Title { get; set; }
+
+    [Parameter]
+    public string? MoreInfo { get; set; }
+
+    [Parameter]
+    public string? UpdatedAtMessage { get; set; }
+
+    [Parameter] public List<(string label, string value)>? Items { get; set; } = new();
+    
+    protected override void OnParametersSet()
+    {
+        BackgroundColor ??= "#12b0c5";
+    }
+}
